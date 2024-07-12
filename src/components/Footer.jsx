ed9146cf6cda8f1e7f6306f74b16f4861743d52a
Lerato Mthimkhulu <mthimkhululerato72@gmail.com>
2024-07-12 21:13:58 +0200

    fixed error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <footer className="py-5 sm:px-10 px-5">
+    <footer className="px-5 py-5 sm:px-10">
       <div className="screen-max-width">
         <div>
-          <p className="font-semibold text-gray text-xs">
+          <p className="text-xs font-semibold text-gray">
             More ways to shop: {' '}
             <span className="underline text-blue">
             Find an Apple iStore {' '}
@@ -17,18 +17,18 @@ const Footer = () => {
             </span>{' '}
             near you.
           </p>
-          <p className="font-semibold text-gray text-xs">
+          <p className="text-xs font-semibold text-gray">
             Or call 031-040-1966
           </p>
         </div>
 
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
-        <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
-          <div className="flex">
+        <div className="flex flex-col justify-between md:flex-row md:items-center">
+          <p className="text-xs font-semibold text-gray">Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <div className="flex flex-wrap">
             {footerLinks.map((link, i) => (
-              <p key={link} className="font-semibold text-gray text-xs">
+              <p key={link} className="text-xs font-semibold text-gray">
                 {link}{' '}
                 {i !== footerLinks.length - 1 && (
                   <span className="mx-2"> | </span>
